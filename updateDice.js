ce5bdbffd6160c35c5b480b9f32c081ac79f332c
sparker <scott.p.parker@gmail.com>
2014-10-26 14:41:11 -0500

    Added boost, setback dice

--- updateDice.js
+++ updateDice.js
@@ -1,9 +1,14 @@
+var SETBACK_WIDTH = 6.8;
+var SETBACK_Y_OFFSET = -1.4;
+var BOOST_WIDTH = 6.8;
+var BOOST_Y_OFFSET = -1.4;
 var PROFICIENCY_WIDTH = 7.6535644;
 var PROFICIENCY_Y_OFFSET = -0.5;
 var ABILITY_WIDTH = 5.734069;
 var ABILITY_Y_OFFSET = 0.0;
 var DIE_BUFFER = 0.01;
 var MAX_DICE = 7;
+var MAX_BOOST = 2;
 
 function forEach(array, fn) {
   var arrayLength = array.length;
@@ -19,6 +24,12 @@ function dieFieldName(prefix, dieType, position) {
 }
 
 function removeAllDice(namePrefix) {
+  for(var i = 1; i <= MAX_BOOST; i++) {
+    this.removeField(dieFieldName(namePrefix, 'Setback', i));
+  }
+  for(var i = 1; i <= MAX_BOOST; i++) {
+    this.removeField(dieFieldName(namePrefix, 'Boost', i));
+  }
   for(var i = 1; i <= MAX_DICE; i++) {
     this.removeField(dieFieldName(namePrefix, 'Proficiency', i));
   }
@@ -70,10 +81,24 @@ function cloneDieField(name, dieType, x, y) {
   return newField;
 }
 
-function setDice(namePrefix, abilities, proficiencies, topRightX, topRightY) {
+function setDice(namePrefix, abilities, proficiencies, boosts, setbacks, topRightX, topRightY) {
   var currentX = topRightX;
 
   removeAllDice(namePrefix);
+
+  for(var i = 1; i <= setbacks; i++) {
+    currentX = currentX - SETBACK_WIDTH;
+    var newName = dieFieldName(namePrefix, 'Setback', i);
+    cloneDieField(newName, 'Setback', currentX, topRightY + SETBACK_Y_OFFSET);
+    currentX = currentX - DIE_BUFFER;
+  }
+
+  for(var i = 1; i <= boosts; i++) {
+    currentX = currentX - BOOST_WIDTH;
+    var newName = dieFieldName(namePrefix, 'Boost', i);
+    cloneDieField(newName, 'Boost', currentX, topRightY + BOOST_Y_OFFSET);
+    currentX = currentX - DIE_BUFFER;
+  }
 
   for(var i = 1; i <= proficiencies; i++) {
     currentX = currentX - PROFICIENCY_WIDTH;
@@ -90,16 +115,36 @@ function setDice(namePrefix, abilities, proficiencies, topRightX, topRightY) {
   }
 }
 
+function parseSkill(rawValue) {
+  var boosts = 0;
+  var setbacks = 0;
+  for(var i = 0; i < rawValue.length; i++) {
+    if(rawValue[i] == '+') {
+      boosts += 1;
+    }
+    if(rawValue[i] == '-') {
+      setbacks += 1;
+    }
+  }
+
+  return {
+    boosts: boosts,
+    setbacks: setbacks,
+    rank: parseInt(rawValue)
+  };
+}
+
 function showDice(namePrefix, skill, characteristic, topRightX, topRightY) {
   var skillField = this.getField(skill);
   var characteristicField = this.getField(characteristic);
-  var skillValue = parseInt(skillField.value);
+  var skill = parseSkill(skillField.value);
+  var skillValue = skill.rank;
   var characteristicValue = parseInt(characteristicField.value);
 
   var min = (skillValue < characteristicValue) ? skillValue : characteristicValue;
   var max = (skillValue > characteristicValue) ? skillValue : characteristicValue;
 
-  setDice(namePrefix, (max - min), min, topRightX, topRightY);
+  setDice(namePrefix, (max - min), min, skill.boosts, skill.setbacks, topRightX, topRightY);
 }
 
 // showDice('CoolSkill', 'Cool', 'Presence', [670.96.., 551.48...]
